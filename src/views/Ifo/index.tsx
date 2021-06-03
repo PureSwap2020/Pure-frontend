c36a6ec9ce6d055b36907732fd320a749136d84e
pureswapdev <pureswapdev@protonmail.com>
2021-06-03 21:36:48 +0800

    add ifo

---
+++ src/views/Ifo/index.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './assets/less/index.less'
+
+const IFO = () => {
+  return (
+    <div>
+      <div className="test-less">
+        hello
+      </div>
+    </div>
+  )
+}
+
+export default IFO
